5b3c196b8ac0cf41e7fb659e63d1ee29d906e757
RomuloSousa1 <romullosousa@outlook.com.br>
2021-09-05 15:45:07 -0300

    Organizando o Container Tranfer e Transaction Feed

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -18,85 +18,66 @@ class Dashboard extends StatelessWidget {
           ),
           Row(
             children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Material(
-                  color: Theme.of(context).primaryColor,
-                  child: InkWell(
-                    onTap: () {
-                      Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (context) => ContactsList(),
-                        ),
-                      );
-                    },
-                    child: Container(
-                      padding: EdgeInsets.all(8.0),
-                      height: 100,
-                      width: 150,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Icon(
-                            Icons.people,
-                            color: Colors.white,
-                            size: 24.0,
-                          ),
-                          Text(
-                            'Contacts',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 16.0,
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
+              _FormTranfer(
+                'Transfer',
+                Icons.monetization_on,
               ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Material(
-                  color: Theme.of(context).primaryColor,
-                  child: InkWell(
-                    onTap: () {
-                      Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (context) => ContactsList(),
-                        ),
-                      );
-                    },
-                    child: Container(
-                      padding: EdgeInsets.all(8.0),
-                      height: 100,
-                      width: 150,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Icon(
-                            Icons.people,
-                            color: Colors.white,
-                            size: 24.0,
-                          ),
-                          Text(
-                            'Transfer',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 16.0,
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
+              _FormTranfer(
+                'Transaction Feed',
+                Icons.description,
               ),
             ],
           )
         ],
+      ),
+    );
+  }
+}
+
+class _FormTranfer extends StatelessWidget {
+  final String nome;
+  final IconData icon;
+
+  _FormTranfer(this.nome, this.icon);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Material(
+        color: Theme.of(context).primaryColor,
+        child: InkWell(
+          onTap: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => ContactsList(),
+              ),
+            );
+          },
+          child: Container(
+            padding: EdgeInsets.all(8.0),
+            height: 100,
+            width: 150,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Icon(
+                  icon,
+                  color: Colors.white,
+                  size: 24.0,
+                ),
+                Text(
+                  nome,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16.0,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
