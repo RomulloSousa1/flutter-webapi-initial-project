c816f796629107eccac7acb9209ba1f71d99cd01
RomulloSousa1 <55188128+RomulloSousa1@users.noreply.github.com>
2021-09-15 15:17:06 -0300

    Criando a tela de transferencias e salvando as trasferencias feitas na web API

--- lib/http/webclient.dart
+++ lib/http/webclient.dart
@@ -27,12 +27,14 @@ class LoggingInterceptor implements InterceptorContract {
   }
 }
 
+final Client client = InterceptedClient.build(
+  interceptors: [LoggingInterceptor()],
+);
+
+const String baseUrl = 'http://192.168.4.14:8080/transactions';
+
 Future<List<Transaction>> findAll() async {
-  final Client cliente = InterceptedClient.build(
-    interceptors: [LoggingInterceptor()],
-  );
-  final Response response =
-      await get(Uri.parse('http://192.168.4.145:8080/transactions')).timeout(
+  final Response response = await get(Uri.parse(baseUrl)).timeout(
     Duration(seconds: 5),
   );
   final List<dynamic> decodedJson = jsonDecode(response.body);
@@ -46,4 +48,37 @@ Future<List<Transaction>> findAll() async {
     transactions.add(transaction);
   }
   return transactions;
+}
+
+Future<Transaction> save(Transaction transaction) async {
+  final Map<String, dynamic> transactionMap = {
+    'value': transaction.value,
+    'contact': {
+      'name': transaction.contact.name,
+      'accountNumber': transaction.contact.accountNumber
+    }
+  };
+
+  final String transactionJson = jsonEncode(transactionMap);
+
+  final Response response = await client.post(
+    Uri.parse(baseUrl),
+    headers: {
+      'Content-type': 'application/json',
+      'password': '1000',
+    },
+    body: transactionJson,
+  );
+
+  Map<String, dynamic> json = jsonDecode(response.body);
+  final Map<String, dynamic> contactJson = json['contact'];
+  return Transaction(
+    json['value'],
+    Contact(
+      0,
+      contactJson['name'],
+      contactJson['accountNumber'],
+    ),
+  );
+
 }
